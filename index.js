917b8a655b8734843f2de80497aede8a36e3eac8
Greg <gregkavvadias@gmail.com>
2023-02-05 19:12:45 +0200

    Refactor JS to work with buttons. Need to do: - Disable the buttons on page load. - Create function to enable buttons by pressing "Start game". - Add a disable method to the giveResult function. - Create a play again function(?)

--- index.js
+++ index.js
@@ -3,76 +3,101 @@ let wins = 0;
 let losses = 0;
 let ties = 0;
 let round = 0;
+let result;
 const choices = ["rock", "paper", "scissors"];
 
-// Functions for player and computer choices
-function computerChoice() {
+// Function computer choice
+let computerSelection = () => {
     return choices[Math.floor(Math.random() * choices.length)];
 }
-function playerChoice() {
-    let userInput = prompt("Choose you weapon!")
-    if(choices.includes(userInput)) {
-        return userInput.toLowerCase();
-    } else if (userInput == null) {
-        playerChoice();
-    } else  {
-        playerChoice();
+
+// Function player choice
+let playerSelection = "";
+
+let rock = document.querySelector(".rock");
+let paper = document.querySelector(".paper");
+let scissors = document.querySelector(".scissors");
+
+rock.addEventListener("click", ()=> {
+    playerSelection = "rock";
+    countScore(playRound(playerSelection, computerSelection()));
+    giveResult();
+});
+paper.addEventListener("click", ()=> {
+    playerSelection = "paper";
+    countScore(playRound(playerSelection, computerSelection()));
+    giveResult();
+});
+scissors.addEventListener("click", ()=> {
+    playerSelection = "scissors";
+    countScore(playRound(playerSelection, computerSelection()));
+    giveResult();
+});
+
+// Game round
+let screen = document.querySelector(".screen");
+let roundCounter = document.querySelector(".round-counter");
+roundCounter.textContent = "Round: " + round;
+
+function playRound(playerSelection, computerSelection) {
+    round++;  
+    if(playerSelection === computerSelection) {
+        screen.textContent = "It's a tie!";
+        return 'Tie';
+    } else if(playerSelection === "rock" && computerSelection === "scissors") {
+        screen.textContent = "You win!";
+        return 'Victory';
+    } else if(playerSelection === "paper" && computerSelection === "rock") {
+        screen.textContent = "You win!";
+        return 'Victory';
+    } else if(playerSelection === "scissors" && computerSelection === "paper") {
+        screen.textContent = "You win!";
+        return 'Victory';
+    } else {
+        screen.textContent = "You lose!";
+        return 'Defeat';
     }
 }
 
-// Game round
-function gameRound(playerSelection, computerSelection) {   
-    if(playerSelection === computerSelection) {
-        alert("It's a tie!");
-        return 'Tie';
-    } else if(playerSelection === "rock" && computerSelection === "scissors") {
-        alert("You win!");
-        return 'Victory';
-    } else if(playerSelection === "paper" && computerSelection === "rock") {
-        alert("You win!");
-        return 'Victory';
-    } else if(playerSelection === "scissors" && computerSelection === "paper") {
-        alert("You win!");
-        return 'Victory';
-    } else 
-        alert("You lose!");
-        return 'Defeat';
-}
-
 // Score counter
-function scoreCounter(result) {
+let winsCounter = document.querySelector(".wins-counter");
+let tiesCounter = document.querySelector(".ties-counter");
+let lossesCounter = document.querySelector(".losses-counter");
+
+function countScore(result) {
     switch (result) {
         case 'Tie':
             ties++;
+            tiesCounter.textContent = "T: " + ties;
             break;
         case 'Victory':
             wins++;
+            winsCounter.textContent = "W: " + wins;
             break;
         case 'Defeat':
             losses++;
+            lossesCounter.textContent = "L: " + losses;
             break;        
     }
 }
 
 // Game result
-function gameResult() {
-    if (ties > wins && ties > losses) {
-        alert("You've tied");
-    } else if (wins > ties && wins > losses) {
-        alert("You've won!");
-    } else alert("You've lost!");
-}
-
-// Game
-function game() {
-    for (let i = 0; i < 5; i++) {
-        let computerSelection = computerChoice();
-        let playerSelection = playerChoice();
-        result = gameRound(playerSelection, computerSelection);
-        scoreCounter(result);
-        round++
-        if (round == 5) {
-            gameResult();
-        }
+let resultScreen = document.querySelector(".result-screen");
+function giveResult() {
+    if (round === 5) {
+        
+        if (ties > wins && ties > losses) {
+            resultScreen.textContent = "You've tied."
+        } else if (wins > ties && wins > losses) {
+            resultScreen.textContent = "You've won."
+        } else resultScreen.textContent = "You've lost."
     }
 }
+
+// Start game function
+// let startGame = document.querySelector(".start-game");
+// startGame.addEventListener("click", startGame);
+
+// function startGame() {
+
+// }
